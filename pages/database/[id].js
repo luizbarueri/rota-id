55e1ddcbf3c1d602e50d5335fed79c9727b0dfc2
Luiz Amaro <luizbarueri@gmail.com>
2022-10-13 23:40:32 -0300

    inserindo link para busca

--- pages/database/[id].js
+++ pages/database/[id].js
@@ -1,5 +1,16 @@
+import { useRouter } from 'next/router';
 const db = require('./db.json')
 
-export default function Database() {
-    return JSON.stringify(db)
+function Database() {
+    const router = useRouter();
+    const id = router.query.id
+
+    return (
+        <div>
+            <h1>Lista de Clientes</h1>
+            <div>ID: {JSON.stringify(db.find((cliente) => cliente.id === id))}</div>
+        </div>
+    )
 }
+
+export default Database;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,20 @@
 
 export default function Home() {
+    
+    const id = '1'
+
     return (
-        <div>Olá, acho que meu logo não pode ser carregado sem css</div>
+        <div>
+            <div>Olá, acho que meu logo não pode ser carregado sem css</div>
+            <ol>
+                <li><a href="./produtos/1">Produto 1</a></li>
+                <li><a href="./produtos/1">Produto 2</a></li>
+                <li><a href={"./database/" + id}>cliente id = {id} em db.json</a></li>
+            </ol>
+            <div>
+                <label>Entre com um ID:</label><input id="id" type="number"></input>
+            </div>
+        </div>
+
     )
 }
